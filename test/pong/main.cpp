ddc28c5cd4fadad0464b446d76a227f0772b1599
stephane.maniaci <stephane.maniaci@9c2135e6-ba31-11de-8dfb-43c0dffa5306>
2009-11-21 16:56:05 +0000

    Ajout d'un TODO

--- test/pong/main.cpp
+++ test/pong/main.cpp
@@ -12,7 +12,7 @@ main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QWidget *window = new QWidget();
 	QVBoxLayout *layout = new QVBoxLayout();
-	
+// TODO: changer les wigets...	
 	QPushButton *button = new QPushButton("Quit");
 	button->connect(button, SIGNAL(clicked()), qApp, SLOT(quit()));
 	window->setLayout(layout);
