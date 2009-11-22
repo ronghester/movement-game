34317faeb74dd75b8496ef13a86f6369636a02c8
stephane.maniaci <stephane.maniaci@9c2135e6-ba31-11de-8dfb-43c0dffa5306>
2009-11-22 01:37:05 +0000

    Oubli des fichiers...

---
+++ trunk/test/gestionsession/menuutilisateur.hpp
@@ -0,0 +1,37 @@
+#ifndef MENUUTILISATEUR_H
+#define MENUUTILISATEUR_H
+
+#include <QtGui>
+#include <QWidget>
+#include <QLayout>
+#include <QPushButton>
+#include <QLabel>
+#include <QLayout>
+#include <QFile>
+#include <QString>
+#include "menusessions.hpp"
+#include "utilisateur.hpp"
+
+class MenuUtilisateur : QWidget
+{
+	Q_OBJECT
+	
+protected:
+	// Utilisateur concerné
+	Utilisateur joueur;
+	
+	// Widgets d'affichage
+	QWidget *window;
+	QLabel *affichagePseudo;
+	QPushButton *retourMenu;
+	QPushButton *quitter;
+public:
+	// Constructeur de la classe
+	MenuUtilisateur(QWidget *parent, Utilisateur joueurA);
+
+// Déclaration des slots
+public slots:
+	void a();
+};
+
+#endif
