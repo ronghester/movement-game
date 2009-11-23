d9c875e6c4047938c9983e2c10ed23c9af9436e8
stephane.maniaci <stephane.maniaci@9c2135e6-ba31-11de-8dfb-43c0dffa5306>
2009-11-23 12:52:09 +0000

    Refactor the code a little bit, next step is keyboard handling.

--- trunk/test/pong/main.cpp
+++ trunk/test/pong/main.cpp
@@ -4,6 +4,8 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QPushButton>
+#include <QGraphicsEllipseItem>
+#include <QDebug>
 
 #include "player.hpp"
 
@@ -13,32 +15,42 @@ main(int argc, char *argv[])
 	QApplication *app = new QApplication(argc, argv);
 	QWidget *window = new QWidget();
 	window->setWindowTitle("Serious Game");
-	window->setFixedSize(640, 480);
-	
-	// Bouton d'arrêt
+
+	QVBoxLayout *layout = new QVBoxLayout(window);
+
+	// quit button
 	QPushButton *button = new QPushButton("Quit");
 	button->connect(button, SIGNAL(clicked()), app, SLOT(quit()));
-	
-	QVBoxLayout *layout = new QVBoxLayout(window);
-	layout->addWidget(button);
-	
-	// Canvas utilisé pour le jeu
-	QGraphicsScene *scene = new QGraphicsScene();
-
-	// Un nouveau joueur Pong, i.e un rectangle
-	Player *p1 = new Player(-500, 0, 20, 100, QBrush(Qt::red));
-	Player *p2 = new Player(scene->width()-50, 0, 20, 100, QBrush(Qt::blue));
-	
-      	scene->addItem(p1);
-	scene->addItem(p2);
-	
 		
-	// Notre vue sur la scène
+	// the game scene
+	QGraphicsScene *scene = new QGraphicsScene();
+	scene->setSceneRect(0, 0, 500, 300);	
+	
+	// the game view
 	QGraphicsView *view = new QGraphicsView(scene, window);
+
+	// the ball
+	QGraphicsEllipseItem *balle = new QGraphicsEllipseItem(scene->width()/2-15, scene->height()/2-15, 30, 30);
+		
+	// two new players = QGraphicsRect.
+	Player *p1 = new Player(0, scene->height()/2-50, 20, 100, QBrush(Qt::blue));
+	Player *p2 = new Player(scene->width()-20, scene->height()/2-50, 20, 100, QBrush(Qt::red));
+
+	// Adding items to the scene
+	scene->addItem(p1);
+	scene->addItem(p2);
+	scene->addItem(balle);
 	
+	// Trying to make p1 grab key events
+	scene->setFocusItem(p1);
+	
+	// Adding the button and the view.
+	layout->addWidget(button);
 	layout->addWidget(view);
-	
+
 	window->show();
 	
 	return app->exec();
 }
+
+
